84032872a2f609ffa812c98760f071dc1dd0f7c8
John Jack Franji <john.jack.franji@sap.com>
2024-03-11 19:47:46 +0100

    add sqllite for test run

--- eventPlanner/settings.py
+++ eventPlanner/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
         "PASSWORD": env("DB_PASSWORD"),
         "HOST": env("DB_HOST"),
         "PORT": env("DB_PORT"),
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "test_db.sqlite3",
     }
 }
 
