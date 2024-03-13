beea19e5b5b4fc7271546a5be271b435fba83757
jeanjackfranji <jeanjack.franji@gmail.com>
2024-03-13 02:11:06 +0100

    Implemented Event Detail Page with Actions + Model Changes

--- eventplannerdb/models.py
+++ eventplannerdb/models.py
@@ -7,8 +7,11 @@ class Event(models.Model):
     location = models.CharField(max_length=255)
     startDateTime = models.DateTimeField()
     endDateTime = models.DateTimeField()
+    description = models.TextField(default="")
     speakers = models.ManyToManyField("Speaker", related_name="events")
     sponsors = models.ManyToManyField("Sponsor", related_name="events")
+    registered_users = models.ManyToManyField(User, related_name="registered_events", blank=True)
+    checkedIn_users = models.ManyToManyField(User, related_name="checkedIn_events", blank=True)
 
 
 class Speaker(models.Model):
@@ -46,9 +49,3 @@ class EventAgenda(models.Model):
     title = models.CharField(max_length=255)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-
-
-class UserCheckIn(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    check_in_time = models.DateTimeField(auto_now_add=True)
